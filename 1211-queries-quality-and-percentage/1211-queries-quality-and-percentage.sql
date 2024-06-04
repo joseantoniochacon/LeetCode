3f63d2adfe987cd01f321434bcd6640de5fed12f
José Antonio Chacón Gutiérrez <138903866+joseantoniochacon@users.noreply.github.com>
2024-06-04 12:11:28 -0600

    Time: 326 ms (100.00%), Space: 0B (100.00%) - LeetHub

---
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+SELECT query_name, ROUND((SUM(rating/position)/COUNT(query_name)),2) AS quality, ROUND((SUM(IF(rating<3,1,0))/COUNT(query_name))*100,2) AS poor_query_percentage FROM Queries WHERE query_name IS NOT NULL GROUP BY query_name;
