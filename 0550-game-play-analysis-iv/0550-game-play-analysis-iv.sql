8b9cebd2440a76ff877d2b8557474b18363c7e25
José Antonio Chacón Gutiérrez <138903866+joseantoniochacon@users.noreply.github.com>
2024-06-07 14:02:30 -0600

    Time: 5680 ms (5.01%), Space: 0B (100.00%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -1,10 +1,2 @@
 # Write your MySQL query statement below
-#SELECT * FROM (SELECT player_id, event_date FROM Activity ORDER BY event_date DESC) a GROUP BY player_id;
-
-#SELECT player_id, event_date FROM Activity ORDER BY event_date DESC
-
-#SELECT DISTINCT(player_id) FROM Activity;
-
-#SELECT player_id,  FROM Activity GROUP BY player_id;
-
 SELECT ROUND((COUNT(*)/(SELECT COUNT(DISTINCT(player_id)) FROM Activity)),2) AS fraction FROM (SELECT player_id, MIN(event_date) AS first_log, (SELECT MIN(event_date) FROM Activity WHERE player_id = a1.player_id AND event_date > (SELECT MIN(event_date) FROM Activity WHERE player_id = a1.player_id)) AS second_log FROM Activity a1 GROUP BY player_id) logs WHERE DATEDIFF(second_log, first_log) = 1;
